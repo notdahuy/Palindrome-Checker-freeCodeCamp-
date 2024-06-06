ba1bbc341eda286ebcb8db5ea06ca4e501dc68c6
notdahuy <giahuyvan0812@gmail.com>
2024-06-06 16:54:04 +0700

    finished

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const inputText = document.getElementById('text-input');
+const inputElement = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
+const resultDiv = document.getElementById('result');
 
+const isPalindrome = (text) => {
+    const cleanText = text.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+    const reversedText = cleanText.split("").reverse().join("");
+    return reversedText === cleanText;
+}
+
+const checkPalindrome = () => {
+    const inputTextValue = inputElement.value;
+    resultDiv.classList.remove("hidden");
+    if (inputTextValue) {
+        if (isPalindrome(inputTextValue)) {
+            resultDiv.innerHTML = `<p class="user-input"><strong>${inputTextValue}</strong> is a palindrome</p>`;
+        } else {
+            resultDiv.innerHTML = `<p class="user-input"><strong>${inputTextValue}</strong> is  not a palindrome</p>`;
+        }
+        inputElement.value = ""; 
+    } else {
+        alert("Please input a value");
+    }
+}
+
+checkBtn.addEventListener('click', checkPalindrome);
